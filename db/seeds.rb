13d9d537039d7c3d58faa2070d663aeed8a3a99a
Matthieu Chouanneau <m.chouanneau@gmail.com>
2022-03-15 14:55:26 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.create(
+    title:  Faker::Mountain.name,
+    content: Faker::Books::Dune.quote,
+  )
+end
+puts 'Finished!'
